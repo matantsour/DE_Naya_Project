d6c6ac71b24032b2dcf5e550fa00c0e5568e9312
Matan Tsour <64172589+matantsour@users.noreply.github.com>
2025-08-14 16:20:53 +0300

    upload_files_to_s3_from_adzuna

--- start_adzuna_extraction_matan.py
+++ start_adzuna_extraction_matan.py
@@ -9,6 +9,42 @@ APP_ID = '223d9f60'
 APP_KEY = '3f4c7510782d76d9edffe71e90119545'
 
 # --- Upload to S3 ---
+def get_job_location_area(job_location_area_list):
+    if not job_location_area_list:
+        return {}
+    if len(job_location_area_list)!=4:
+        return {}
+    return {'Country':job_location_area_list[0],'State':job_location_area_list[1],'County':job_location_area_list[2],'City':job_location_area_list[3]}
+
+def flatten_job(job):
+    # Flatten nested fields category, company, location with safe defaults
+    area_dict = get_job_location_area(job.get("location", {}).get("area"))
+    return {
+        "title": job.get("title"),
+        "salary_max": job.get("salary_max"),
+        "salary_min": job.get("salary_min"),
+        "salary_is_predicted": job.get("salary_is_predicted"),
+        "description": job.get("description"),
+        "redirect_url": job.get("redirect_url"),
+        "id": job.get("id"),
+        "created": job.get("created"),
+        'contract_type': job.get('contract_type'),
+        'contract_time': job.get('contract_time'),
+        # Flatten category
+        "category_label": job.get("category", {}).get("label"),
+        "category_tag": job.get("category", {}).get("tag"),
+        # Flatten company
+        "company_display_name": job.get("company", {}).get("display_name"),
+        # Flatten location
+        "location_display_name": job.get("location", {}).get("display_name"),
+        'location_Country':area_dict.get('Country'),
+        'location_State': area_dict.get('State'),
+        'location_County': area_dict.get('County'),
+        'location_City': area_dict.get('City'),
+        'latitude': job.get('latitude'),
+        'longitude': job.get('longitude'),
+    }
+
 def upload_results_to_aws_bucket(results, job_title, state, city):
     bucket_name = 'naya-project-job-ads'
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -17,19 +53,21 @@ def upload_results_to_aws_bucket(results, job_title, state, city):
     month = datetime.now().strftime("%m")
     day = datetime.now().strftime("%d")
 
-    key = f'data/raw/adzuna/{year}/{month}/{day}/{timestamp}.json'
+    key = f'data/raw/adzuna/{year}/{month}/{day}/{timestamp}_set1.json'
+    flattened_jobs = [flatten_job(job) for job in results.get("results", [])]
+    # Convert each dict to JSON line
+    json_lines = "\n".join(json.dumps(job) for job in flattened_jobs)
 
-    json_string = json.dumps(results, indent=2)
     s3 = boto3.client('s3')
 
     try:
         s3.put_object(
             Bucket=bucket_name,
             Key=key,
-            Body=json_string,
+            Body=json_lines,
             ContentType='application/json'
         )
-        print(f"✅ Uploaded {len(results.get('results', []))} jobs: {job_title} in {city}, {state}")
+        print(f"✅ Uploaded {len(flattened_jobs)} flattened jobs: {job_title} in {city}, {state}")
     except Exception as e:
         print(f"❌ S3 Upload failed for {job_title} in {city}, {state}: {e}")
 
